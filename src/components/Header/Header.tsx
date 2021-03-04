93946c05b8b7d5e8471a70b80542872020d5ed0e
vladmoskov <perishvlad@gmail.com>
2021-03-04 21:24:46 +0200

    Auth-reducer Header/HeaderContainer finished

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import s from './Header.module.css';
 import {NavLink} from "react-router-dom";
+import logo from './../../images/logo.svg'
 
 type PropsType = {
     isAuth: boolean
     login: string | null
 }
 
-const Header: React.FC<PropsType> = ({isAuth, login}) => {
+export const Header: React.FC<PropsType> = ({isAuth, login}) => {
     return (
         <header className={s.header}>
             <div className={s.logo}>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Ubisoft_logo.svg"  alt={""}/>
+                <img src={logo}  alt={""}/>
             </div>
             <h1>
                 { isAuth ? login : <NavLink to='/login'>Login In</NavLink> }
             </h1>
         </header>
-    );
+    )
 }
-
-export default Header;
--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import Header from "./Header";
+import {Header} from "./Header";
 import {useDispatch} from "react-redux";
 import {getAuthUserData} from '../../redux/Auth-reducer';
 import {useSelector} from "../common/hooks";
